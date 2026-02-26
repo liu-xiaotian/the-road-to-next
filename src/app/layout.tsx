570befdb4ae79c6882dd244eb116e43491c0bc22
tt <xiaotian3303@outlook.com>
2026-02-26 14:14:04 +0800

    Header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Link from "next/link";
-import { homePath, ticketsPath } from "@/paths";
-import { Button, buttonVariants } from "@/components/ui/button";
-import { LucideKanban } from "lucide-react";
+import { Header } from "@/components/header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,32 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <nav
-          className="
-          supports-backdrop-blur:bg-background/60
-          flxed left-0 right-0 top-0 z-20
-          border-b bg-background/95 backdrop-blur
-          w-full flex py-2.5 px-5 justify-between
-        "
-        >
-          <div>
-            <Link
-              href={homePath()}
-              className={buttonVariants({ variant: "ghost" })}
-            >
-              <LucideKanban />
-              <h1 className="ml-2 text-lg font-semibold">TicketBounty</h1>
-            </Link>
-          </div>
-          <div>
-            <Link
-              href={ticketsPath()}
-              className={buttonVariants({ variant: "default" })}
-            >
-              Tickets
-            </Link>
-          </div>
-        </nav>
+        <Header />
         <main
           className="
           min-h-screen flex-1
