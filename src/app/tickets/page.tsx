212ca2900e6c7df612bfd3a014834ccd78c38f94
tt <xiaotian3303@outlook.com>
2026-02-26 14:08:14 +0800

    Heading

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -10,13 +10,12 @@ import {
   CardDescription,
   CardContent,
 } from "@/components/ui/card";
-import { Separator } from "@/components/ui/separator";
 import {
   LucideCircleCheckBig,
   LucideFileText,
   LucidePencil,
 } from "lucide-react";
-
+import { Heading } from "@/components/heading";
 const TICKET_ICONS = {
   OPEN: <LucideFileText />,
   IN_PROGRESS: <LucidePencil />,
@@ -26,13 +25,10 @@ const TICKET_ICONS = {
 const TicketsPage = () => {
   return (
     <div className="flex-1 flex flex-col gap-y-8">
-      <div>
-        <h2 className="text-3xl font-bold tracking-tight">Tickets Page</h2>
-        <p className="text-sm text-muted-foreground">
-          All your tickets at one place
-        </p>
-      </div>
-      <Separator />
+      <Heading
+        title={"Tickets Page"}
+        description={"All your tickets at one place"}
+      />
       <div className="flex-1 flex flex-col items-center gap-y-4 animate-fade-in-from-top">
         {initialTickets.map((ticket) => (
           <Card key={ticket.id} className="w-full max-w-[420px]">
