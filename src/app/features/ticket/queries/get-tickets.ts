ac2e4624375a1e87e769c62f990f8dd0ba73583b
tt <xiaotian3303@outlook.com>
2026-03-01 00:03:10 +0800

    database queries

--- src/app/features/ticket/queries/get-tickets.ts
+++ src/app/features/ticket/queries/get-tickets.ts
@@ -1,10 +1,8 @@
-import { initialTickets } from "@/data";
-import { Ticket } from "../types";
-
-export const getTickets = async (): Promise<Ticket[]> => {
-  await new Promise((resolve) => setTimeout(resolve, 2000));
-
-  return new Promise((resolve) => {
-    resolve(initialTickets);
+import prisma from "@/lib/prisma";
+export const getTickets = async () => {
+  return await prisma.ticket.findMany({
+    orderBy: {
+      createdAt: "desc",
+    },
   });
 };
