e9c038ccefe04601f23b7f7a0568cb1b0810cb42
tt <xiaotian3303@outlook.com>
2026-03-02 22:22:29 +0800

    配置胜于组合

---
+++ src/components/card-compact.tsx
@@ -0,0 +1,37 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "./ui/card";
+
+type CardCompactProps = {
+  title: string;
+  description: string;
+  content: React.ReactNode;
+  footer?: React.ReactNode;
+  className?: string;
+};
+
+const CardCompact = ({
+  title,
+  description,
+  content,
+  footer,
+  className,
+}: CardCompactProps) => {
+  return (
+    <Card className={className}>
+      <CardHeader>
+        <CardTitle>{title}</CardTitle>
+        <CardDescription>{description}</CardDescription>
+      </CardHeader>
+      <CardContent>{content}</CardContent>
+      {footer && <CardFooter>{footer}</CardFooter>}
+    </Card>
+  );
+};
+
+export { CardCompact };
