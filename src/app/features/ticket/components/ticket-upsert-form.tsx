29046d412d814336c2825c897a1f96a6bf25993e
tt <xiaotian3303@outlook.com>
2026-03-03 17:18:40 +0800

    Toast 气泡反馈

--- src/app/features/ticket/components/ticket-upsert-form.tsx
+++ src/app/features/ticket/components/ticket-upsert-form.tsx
@@ -9,6 +9,7 @@ import { useActionState, useEffect } from "react";
 import { FieldErrors } from "@/components/form/field-error";
 import { EMPTY_ACTION_STATE } from "@/components/form/utils/to-action-state";
 import { useActionFeedback } from "@/components/form/hooks/use-action-feedback";
+import { toast } from "sonner";
 
 type TicketUpsertFormProps = {
   ticket?: Ticket;
@@ -21,10 +22,10 @@ const TicketUpsertForm = ({ ticket }: TicketUpsertFormProps) => {
   );
   useActionFeedback(actionState, {
     onSuccess: ({actionState}) => {
-      console.log(actionState.message);
+      toast.success(actionState.message);
     },
     onError: ({actionState}) => {
-      console.log(actionState.message);
+      toast.error(actionState.message);
     },
   });
 
